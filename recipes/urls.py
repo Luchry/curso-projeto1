c509c384dbd2e3eb6ca05bde8889a0ce8a52b6aa
lucas Soares <lucksouza275@gmail.com>
2022-07-19 10:39:07 -0300

    remover contato e sobre

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from recipes.views import about, contact, home
+from recipes.views import home
 
 urlpatterns = [
-    path('sobre/', about),
+
     path('', home),
-    path('contato/', contact),
+
 
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -10,15 +10,4 @@ def home(request):
     })
 
 
-def about(request):
-
-    return HttpResponse('ABOUT')
-
-
-def contact(request):
-
-    return render(request, 'recipes/contact.html', context={
-        'telefone': '13997861119',
-    })
-
 # Create your views here.
