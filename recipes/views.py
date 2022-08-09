d4fdf26b528a985570bf1fd6f4ba4456a8e5f0e7
lucas Soares <lucksouza275@gmail.com>
2022-08-09 15:19:00 -0300

    alteração nos links

--- recipes/views.py
+++ recipes/views.py
@@ -1,18 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import path
+from utils.recipes.factory import make_recipe
 
 
 def home(request):
 
     return render(request, 'recipes/pages/home.html', context={
-        'name': 'lucas soares',
+        'recipes': [make_recipe() for _ in range(9)],
     })
 
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Luiz Otávio',
+        'recipe': make_recipe(),
+        'is_detail_page': True,
     })
 
 
